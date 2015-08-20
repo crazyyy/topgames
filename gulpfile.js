dce89b4d2e5bc6c021c5873b08d13458b1c25c7b
crazyyy <crazyyy@gmail.com>
2015-08-20 15:50:57 +0300

    html ready

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,8 @@ var gulp = require('gulp'),
 
 /* if work with html == true, else - false */
 var htmlOWp = true,
-  wpThemeName = 'wp-framework',
-  wpDomain = 'wp-framework.dev';
+  wpThemeName = 'wp-igrali',
+  wpDomain = 'igralli.com';
 
 var AUTOPREFIXER_BROWSERS = [
   'ie >= 8', 'ie_mob >= 10', 'ff >= 20', 'chrome >= 24', 'safari >= 5', 'opera >= 12', 'ios >= 7', 'android >= 2.3', '> 1%', 'last 4 versions', 'bb >= 10'
