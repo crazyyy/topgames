cd127126dab0c82a365fdcf297de294cd0a08834
crazyyy <crazyyy@gmail.com>
2015-08-20 17:15:21 +0300

    footer

--- wordpress/wp-content/themes/wp-igrali/sidebar.php
+++ wordpress/wp-content/themes/wp-igrali/sidebar.php
@@ -11,98 +11,46 @@
   <div class="vbox">
     <h2>Новые игры</h2>
     <ol class="games">
-      <li>
-        <noindex>
-          <a rel="nofollow" href="http://xn-----ilcsbptiplpzyo.xn--p1ai/?p=13">
-            <img src="img/timthumb.php" alt="">
-            <span>Neverwinter – Новинка MMORPG!</span>
-          </a>
-        </noindex>
-      </li>
-      <li>
-        <noindex>
-          <a rel="nofollow" href="http://xn-----ilcsbptiplpzyo.xn--p1ai/?p=1855">
-            <img src="img/timthumb(1).php" alt="">
-            <span>Tribal Wars – Война Племен! Топ 2015!</span>
-          </a>
-        </noindex>
-      </li>
-      <li>
-        <noindex>
-          <a rel="nofollow" href="http://xn-----ilcsbptiplpzyo.xn--p1ai/?p=3054">
-            <img src="img/timthumb(2).php" alt="">
-            <span>Dragon Knight –  MMO! Бета Тест!</span>
-          </a>
-        </noindex>
-      </li>
-      <li>
-        <noindex>
-          <a rel="nofollow" href="http://xn-----ilcsbptiplpzyo.xn--p1ai/?p=3835">
-            <img src="img/timthumb(3).php" alt="">
-            <span>WorldofWarships – Военно Морская MMO</span>
-          </a>
-        </noindex>
-      </li>
-      <li>
-        <noindex>
-          <a rel="nofollow" href="http://xn-----ilcsbptiplpzyo.xn--p1ai/?p=2109">
-            <img src="img/timthumb(4).php" alt="">
-            <span>ВойныПрестолов – Стратегия 2015!</span>
-          </a>
-        </noindex>
-      </li>
+    <?php query_posts("showposts=5&cat=-1"); ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <li>
+          <noindex>
+            <a rel="nofollow" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+              <?php if ( has_post_thumbnail()) :
+                the_post_thumbnail('medium');
+              else: ?>
+                <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+              <?php endif; ?>
+              <span><?php the_title(); ?></span>
+            </a>
+          </noindex>
+        </li>
+      <?php endwhile; endif; ?>
+    <?php wp_reset_query(); ?>
     </ol>
   </div>
-
-
 
   <div class="vbox">
     <h2>Новости и статьи</h2>
     <ol class="games">
+      <?php query_posts("showposts=4&cat=1"); ?>
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
       <li>
         <noindex>
-          <a rel="nofollow" href="http://xn-----ilcsbptiplpzyo.xn--p1ai/?p=4396">
-            <img src="img/timthumb(5).php" alt="">
-            <span>Новое обновление для Gloria Victis</span>
+          <a rel="nofollow" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+              <?php if ( has_post_thumbnail()) :
+                the_post_thumbnail('medium');
+              else: ?>
+                <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+              <?php endif; ?>
+            <span><?php the_title(); ?></span>
           </a>
         </noindex>
       </li>
-      <li>
-        <noindex>
-          <a rel="nofollow" href="http://xn-----ilcsbptiplpzyo.xn--p1ai/?p=4392">
-            <img src="img/timthumb(6).php" alt="">
-            <span>Обновление для Shroud of the Avatar</span>
-          </a>
-        </noindex>
-      </li>
-      <li>
-        <noindex>
-          <a rel="nofollow" href="http://xn-----ilcsbptiplpzyo.xn--p1ai/?p=4388">
-            <img src="img/timthumb(7).php" alt="">
-            <span>Масштабное обновление для TERA</span>
-          </a>
-        </noindex>
-      </li>
-      <li>
-        <noindex>
-          <a rel="nofollow" href="http://xn-----ilcsbptiplpzyo.xn--p1ai/?p=4383">
-            <img src="img/timthumb(8).php" alt="">
-            <span>Новое обновление для Moonlight Blade</span>
-          </a>
-        </noindex>
-      </li>
-      <li>
-        <noindex>
-          <a rel="nofollow" href="http://xn-----ilcsbptiplpzyo.xn--p1ai/?p=4378">
-            <img src="img/timthumb(9).php" alt="">
-            <span>Новое обновление для Skyforge</span>
-          </a>
-        </noindex>
-      </li>
+      <?php endwhile; endif; ?>
+      <?php wp_reset_query(); ?>
     </ol>
   </div>
-
-
 
 </div><!-- sidebar -->
 
